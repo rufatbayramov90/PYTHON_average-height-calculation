b47672e17aab92089e097064f19d1db0f555af7b
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-06 12:00:33 +0400

    number of elements

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ student_heights = input().split()
 
 for n in range(0, len(student_heights)):
     student_heights[n]  = int(student_heights[n])
+
 total_height = 0
 for height in student_heights:
     total_height += height
-print(total_height)
+
+number_student = 0
+for student in student_heights:
+    number_student += 1
+print(number_student)
+
+
 
