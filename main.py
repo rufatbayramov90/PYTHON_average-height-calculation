16b24384ad30fd4e250a42b5f4b3a566108dc30a
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-06 11:58:17 +0400

    sum of elements

---
+++ main.py
@@ -0,0 +1,9 @@
+student_heights = input().split()
+
+for n in range(0, len(student_heights)):
+    student_heights[n]  = int(student_heights[n])
+total_height = 0
+for height in student_heights:
+    total_height += height
+print(total_height)
+
